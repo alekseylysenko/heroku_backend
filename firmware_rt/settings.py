aca0a313b75698387257622628ab8df3d56072da
DESKTOP-NMFVT17\lysen <lysenko-a@mail.ru>
2021-02-22 10:16:09 +0300

    34fg

--- firmware_rt/settings.py
+++ firmware_rt/settings.py
@@ -127,6 +127,6 @@ USE_TZ = True
 import mimetypes
 mimetypes.add_type("text/css", ".css", True)
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join('staticfiles')
 STATIC_URL = 'staticfiles/'
 
