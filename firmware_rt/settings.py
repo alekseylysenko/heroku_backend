9d604d776a55a01b9413fc4940c61f0c9f0c909f
DESKTOP-NMFVT17\lysen <lysenko-a@mail.ru>
2021-02-22 10:33:24 +0300

    34fg

--- firmware_rt/settings.py
+++ firmware_rt/settings.py
@@ -128,5 +128,6 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/staticfiles/'
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 django_heroku.settings(locals())
