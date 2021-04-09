cab0a2789a79370a2a9911ed53db8340054329a9
alekseylysenko <lysenko-a@mail.ru>
2021-04-09 18:16:33 +0300

    update api-key

--- firmware/api.py
+++ firmware/api.py
@@ -1,14 +1,16 @@
 from .models import Firmware, Category, Technology
 from rest_framework import viewsets, permissions
 from .serializers import FirmwareSerializer, CategorySerializer, TechnologySerializer
+from rest_framework.authentication import BasicAuthentication
 
 
 class FirmwareViewSet(viewsets.ModelViewSet):
     queryset = Firmware.objects.all()
     ordering = ['id']
     ordering_fields = ['id', 'title']
+    authentication_classes = (BasicAuthentication,)
     permission_classes = [
-        permissions.AllowAny
+        permissions.IsAuthenticatedOrReadOnly
     ]
     serializer_class = FirmwareSerializer
 
@@ -17,8 +19,9 @@ class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     ordering = ['id']
     ordering_fields = ['id', 'title']
+    authentication_classes = (BasicAuthentication,)
     permission_classes = [
-        permissions.AllowAny
+        permissions.IsAuthenticatedOrReadOnly
     ]
     serializer_class = CategorySerializer
 
@@ -26,7 +29,7 @@ class CategoryViewSet(viewsets.ModelViewSet):
 class TechnologyViewSet(viewsets.ModelViewSet):
     queryset = Technology.objects.all().order_by('id')
     permission_classes = [
-        permissions.AllowAny
+        permissions.IsAuthenticatedOrReadOnly
     ]
     serializer_class = TechnologySerializer
 
