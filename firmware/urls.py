15d3ac490f37a7b2a0dcb8a17e4f0e7606114cbe
alekseylysenko <lysenko-a@mail.ru>
2021-07-05 08:15:45 +0300

    add  generic__e

--- firmware/urls.py
+++ firmware/urls.py
@@ -5,7 +5,7 @@ routers = routers.DefaultRouter()
 routers.register('api/firmware', FirmwareViewSet, 'firmware'),
 routers.register('api/category', CategoryViewSet, 'category'),
 routers.register('api/technology', TechnologyViewSet, 'technology')
-routers.register('api/posts', PostViewSet.as_view(), 'posts')
+routers.register('api/posts', PostViewSet, 'posts')
 routers.register('api/category_post', CategoryPostViewSet, 'category_post')
 routers.register('api/chat', ChatViewSet, 'chat')
 urlpatterns = routers.urls
