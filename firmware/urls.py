cc195d25d122c9f47c7790410e12b3e3cda19bdf
alekseylysenko <lysenko-a@mail.ru>
2021-07-05 08:53:16 +0300

    add  generic__e

--- firmware/urls.py
+++ firmware/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
 from rest_framework import routers
+
+from . import api
 from .api import FirmwareViewSet, CategoryViewSet, TechnologyViewSet, PostViewSet, CategoryPostViewSet, ChatViewSet
 
 routers = routers.DefaultRouter()
 routers.register('api/firmware', FirmwareViewSet, 'firmware'),
 routers.register('api/category', CategoryViewSet, 'category'),
 routers.register('api/technology', TechnologyViewSet, 'technology')
+routers.register('api/posts/', PostViewSet.as_view(), 'posts')
 routers.register('api/category_post', CategoryPostViewSet, 'category_post')
 routers.register('api/chat', ChatViewSet, 'chat')
-routers.register('api/posts/', PostViewSet.as_view(), 'posts')
+urlpatterns = routers.urls
 
